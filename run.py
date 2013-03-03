38bd1d6a2bf15ebca0b7195c2c470a8993a3b291
Philipp <fsfeel@gmail.com>
2013-03-04 00:37:56 +0400

    .

--- run.py
+++ run.py
@@ -10,10 +10,16 @@ libtcod.sys_set_fps(60)
 libtcod.console_set_custom_font('data/fonts/dejavu16x16_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(80, 50, 'python/libtcod tutorial', False)
 
+# c1 = libtcod.Color(255, 255, 255)
+# libtcod.console_set_default_background(0, c1)
+# libtcod.console_clear(0)
 
 world_map = [
 	['.'] * 10,
 ] * 10
+
+class color (object):
+	grass = libtcod.Color(50, 150, 50)
 
 key = libtcod.Key()
 mouse = libtcod.Mouse()
@@ -25,10 +31,14 @@ while not libtcod.console_is_window_closed():
 	if mouse.lbutton_pressed:
 		print "left mouse, cell:", mouse.cx, mouse.cy
 
+
 	for y, row in enumerate(world_map):
 		for x, cell in enumerate(row):
 			libtcod.console_put_char(0, x + 1, y + 1, cell, libtcod.BKGND_NONE)
+			libtcod.console_set_char_foreground(0, x + 1, y + 1, color.grass)
+			# libtcod.console_put_char_ex(0, x + 1, y + 1, cell, color.grass, 0)
 
 	libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+
 
 	libtcod.console_flush()
