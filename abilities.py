9fe1d444bd9fa9988d9dfb89cf6eea29c684cd92
tosh <tosh.the@gmail.com>
2013-03-14 22:39:05 +0400

    abilities base class

---
+++ abilities.py
@@ -0,0 +1,14 @@
+import util
+
+class Ability (util.Entity):
+	action_time = 0
+	
+	def __init__ (self, target, *args):
+		super(Ability, self).__init__(*args)
+		self.target = target
+	
+	def use (self):
+		self.state.entities.append(self)
+		def rollback ():
+			self.state.entities.remove(self)
+		self.state.timers.start_run_once(1000, rollback)	
